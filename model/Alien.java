edcda9904ec62baf826779fa2e6a643df5ba380e
BenBartoschik <Ben.Bartoschik@Student.HTW-Berlin.de>
2026-01-11 13:34:19 +0100

    Mother Alien class

--- model/Alien.java
+++ model/Alien.java
@@ -2,8 +2,30 @@ package model;
 
 public abstract class Alien {
 
-    // Bitte serialVersionUID beibehalten, damit die Klasse bei der
-    // Speicherung als Datei (Serialisierung) und beim Laden (Deserialisierung)
-    // konsistent bleibt und Versionierungsprobleme vermieden werden.
     private static final long serialVersionUID = 1729389822767173584L;
+
+    protected String name;
+    protected int lifePoints;
+    protected boolean friendly;
+    protected String greeting;
+
+    public Alien(String name, int lifePoints, boolean friendly, String greeting) {
+        this.name = name;
+        this.lifePoints = lifePoints;
+        this.friendly = friendly;
+        this.greeting = greeting;
+    }
+
+    public int takeDamage(int amount) {
+        this.lifePoints -= amount;
+        return this.lifePoints;
+    }
+
+    public boolean isDefeated() {
+        return this.lifePoints <= 0;
+    }
+
+    public  abstract int doDamage();
+
+
 }
