333bb94dbecdac4c32f48ee1f1dc553ee2e38446
Emmanuel Bessong Ngam <emmanuelbessong@icloud.com>
2026-01-13 20:02:56 +0100

    Klassen angepasst

--- model/Hero.java
+++ model/Hero.java
@@ -10,6 +10,44 @@ public class Hero implements Serializable {
     int experiencePoints;
     Lecturer[] signedExerciseLeaders;
 
+    public void takeDamage(int amount){
+
+        healthPoints = healthPoints - amount;
+
+        if(healthPoints < 0){
+            healthPoints = 0;
+        }
+
+    }
+    public void regenerate(boolean longRest){
+
+        if(longRest == false){
+            healthPoints = healthPoints + 3;
+        }
+        if(longRest == true){
+            healthPoints = healthPoints + 10;
+        }
+        if (healthPoints > 50){
+            healthPoints = 50;
+        }
+    }
+    public boolean flee(){
+
+        double wahrscheinlichkeit = Math.random();
+        if(wahrscheinlichkeit < 0.42){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public int attack(){
+        
+
+
+    }
+
+
+
     // Bitte serialVersionUID beibehalten, damit die Klasse bei der
     // Speicherung als Datei (Serialisierung) und beim Laden (Deserialisierung)
     // konsistent bleibt und Versionierungsprobleme vermieden werden.
